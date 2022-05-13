268d2c98742ad5ae47a2c628c1c653bd4f501f7c
Unnat Das <udas4153@gmail.com>
2022-05-14 01:19:52 +0530

    initial commit

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("working");
-let project_card = document.querySelectorAll('.project_card');
-
-project_card.forEach( (pr) =>{
-    pr.addEventListener('click',()=>{
-        console.log("click");
-        pr.setAttribute("style", "background-color: white !important; z-index: 100000000;");
-    })
-})
